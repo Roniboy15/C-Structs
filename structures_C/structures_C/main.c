11126153762202c6912c155cc485a41c6d16acc1
Your Name <you@example.com>
2022-12-20 12:42:55 +0200

    lunch break

--- structures_C/structures_C/main.c
+++ structures_C/structures_C/main.c
@@ -6,7 +6,8 @@
 //
 
 #include <stdio.h>
-
+#include <math.h>
+/*
 typedef struct Date {
     int day, month, year;
 } date;
@@ -23,3 +24,31 @@ int main(int argc, const char * argv[]) {
 
     return 0;
 }
+*/
+
+//targilim 20.12.23
+
+struct points{
+    float x;
+    float y;
+};
+typedef struct points poin;
+
+float merjak(poin p,poin t){
+    float resp=sqrt(pow(p.x-t.x,2)+ (pow(p.y-t.y,2)));
+    return resp;
+}
+
+int main()
+{
+    float resp;
+    poin p,t;
+    p.x=1.0;
+    t.x=2.5;
+    p.y=3.0;
+    t.y=3.0;
+    resp=merjak(p,t);
+    printf("%f",resp);
+    return 0;
+}
+//d=√((x2 – x1)² + (y2 – y1)²).
